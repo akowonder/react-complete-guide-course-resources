03146ef21707fcfc116abc9a7bbcdbd6530a0b3e
Afolabi Ayo-Iyanu <53617923+akowonder@users.noreply.github.com>
2025-02-15 22:11:35 +0100

    Update App.jsx

--- code/03 React Essentials/04-html-attributes-dynamically-image-files/src/App.jsx
+++ code/03 React Essentials/04-html-attributes-dynamically-image-files/src/App.jsx
@@ -1,3 +1,5 @@
+//proper convetion for adding an image in react
+//reacImg is a name assigned by the user/me
 import reactImg from './assets/react-core-concepts.png';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
@@ -8,7 +10,9 @@ function genRandomInt(max) {
 
 function Header() {
   const description = reactDescriptions[genRandomInt(2)];
-
+  //you add dynamic values to react by putting the value in curly brackets
+  //evrytime the program refreshes a differnt value for our array is displayed
+  //images can also be added to react as dynamic values
   return (
     <header>
       <img src={reactImg} alt="Stylized atom" />
