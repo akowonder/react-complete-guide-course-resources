6af129e06b0e0165444190f719e1c738802f6445
Afolabi Ayo-Iyanu <53617923+akowonder@users.noreply.github.com>
2025-02-16 16:38:22 +0100

    Update TabButton.jsx

--- code/03 React Essentials/10-reacting-to-events/src/components/TabButton.jsx
+++ code/03 React Essentials/10-reacting-to-events/src/components/TabButton.jsx
@@ -2,7 +2,8 @@ export default function TabButton({ children }) {
   function handleClick() {
     console.log('Hello World!');
   }
-
+  //in react you pass your event handlers which issa function as a value.
+  //handleClick is passed as a value and then executed by react when a click occurs
   return (
     <li>
       <button onClick={handleClick}>{children}</button>
